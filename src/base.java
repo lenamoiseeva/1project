4e02b5167939c8d9285d1f7f314dae36d1e5d384
lenamoiseeva <40036816+lenamoiseeva@users.noreply.github.com>
2018-06-12 19:45:38 +0300

    Update base.java

--- src/base.java
+++ src/base.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 /**@author Lena Moiseeva
  * @return  the sum of numbers*/
 public class base {
-    //just a simple calcularor
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double fn, sn, result;
-
+        //Вводим первое число
         System.out.println("Enter the first number: ");
         fn = scanner.nextDouble();
+        //Вводим второе число
         System.out.println("Enter the second number: ");
         sn = scanner.nextDouble();
-
+        
         result = fn + sn;
-
+        //Выводим сумму чисел с округлением
         System.out.printf("Sum is: = %.4f", result);
     }
 }
