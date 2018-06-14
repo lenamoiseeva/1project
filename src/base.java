3b8fbd6ecb2b6bd4e416afca4aac09f73d9ed5fa
lenamoiseeva <40036816+lenamoiseeva@users.noreply.github.com>
2018-06-14 19:33:16 +0300

    Update base.java

--- src/base.java
+++ src/base.java
@@ -1,19 +1,67 @@
 import java.util.Scanner;
-/**@author Lena Moiseeva
-*/
+/**
+ * @author Lena Moiseeva
+ * calcularor
+ * search for the longest word in the array
+ */
 public class base {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double fn, sn, result;
-        //Вводим первое число
-        System.out.println("Enter the first number: ");
-        fn = scanner.nextDouble();
-        //Вводим второе число
-        System.out.println("Enter the second number: ");
-        sn = scanner.nextDouble();
-        
-        result = fn + sn;
-        //Выводим сумму чисел с округлением
-        System.out.printf("Sum is: = %.4f", result);
+        System.out.println("Select action: 1 - Calculator , 2 - Find max word in the array");
+        int choice = scanner.nextInt();
+        //выбор: 1 - Калькулятор, 2 - Поиск максимального слова в массиве.
+        if (choice == 1) {
+            double fn, sn, result;
+            System.out.println("Enter the first number: ");
+            fn = scanner.nextDouble();
+            System.out.println("Enter the second number: ");
+            sn = scanner.nextDouble();
+            System.out.println("Enter one of the options (*, /, +, -): ");
+            String operation = scanner.next();
+            switch (operation) {
+                case "*": {
+                    result = fn * sn;
+                    System.out.printf("Result = %.4f", result);
+                    break;
+                }
+                case "/": {
+                    result = fn / sn;
+                    System.out.printf("Result = %.4f", result);
+                    break;
+                }
+                case "+": {
+                    result = fn + sn;
+                    System.out.printf("Result = %.4f", result);
+                    break;
+                }
+                case "-": {
+                    result = fn - sn;
+                    System.out.printf("Result = %.4f", result);
+                    break;
+                }
+                default:
+                    System.out.println("Invalid value");
+            }
+        }
+        else if (choice == 2) {
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("Enter the dimension of the array:");
+            int a = scanner1.nextInt();
+            String b[] = new String[a];
+
+            for (int i = 0; i < b.length; i++) {
+                System.out.println("Enter the word №" + i);
+                String c = scanner.next();
+                b[i] = c;
+            }
+            String Word = b[0];
+            int max = Word.length();
+            for (int i = 0; i < b.length; i++) {
+                if (max < b[i].length()) {
+                    Word = b[i];
+                }
+            }
+            System.out.println("The longest word is " + Word);
+        }
     }
 }
